63be3f5afcdd621396ce864616286c547cb9d17a
panzert30 <55293857+panzert30@users.noreply.github.com>
2019-09-20 12:58:14 -0400

    Delete commit.bat

--- commit.bat
+++
@@ -1,6 +0,0 @@
-+echo "# oulds_a_reidy_s_first_essay_about_us" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/EquanoxDragon/oulds_a_reidy_s_first_essay_about_us.git
-git push -u origin master
